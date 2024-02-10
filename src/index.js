665e4a200a2e67228eb8d5b99298a8cf7d496f8d
Alex <korte@clemson.edu>
2024-02-10 17:15:34 -0500

    Add Font Stuff

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
-import './fonts/BackIssuesRegular.otf'
 import App from './app';
 
 const root = ReactDOM.createRoot(
